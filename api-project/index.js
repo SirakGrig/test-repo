1a66a2f1b73884be7b8d08111c7a9423c50229b0
Sirak_Grigoryan <sirak.grigoryan@innorise.com>
2026-04-20 14:55:24 +0400

    changes

--- api-project/index.js
+++ api-project/index.js
@@ -7,7 +7,7 @@ const PORT = 3001;
 app.get("/health", (req, res) => {
     res.json({
         status: "ok",
-        message: "API is running 4 commit",
+        message: "API is running 5 commit",
         time: new Date().toISOString()
     });
 });
