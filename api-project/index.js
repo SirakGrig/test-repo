79690a1caa79fb6dcab71e4dd1d73c12e34777cb
Sirak_Grigoryan <sirak.grigoryan@innorise.com>
2026-04-20 14:47:38 +0400

    api added

---
+++ api-project/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3001;
+
+// simple health check
+app.get("/health", (req, res) => {
+    res.json({
+        status: "ok",
+        message: "API is running 2 commit",
+        time: new Date().toISOString()
+    });
+});
+
+
+// simple test route
+app.get("/hello", (req, res) => {
+    res.json({
+        message: "Hello from Express API 🚀"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
