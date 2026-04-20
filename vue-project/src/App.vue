2de3c61e67808b68fa0fa89273f50fe7e4986879
Sirak_Grigoryan <sirak.grigoryan@innorise.com>
2026-04-20 11:58:57 +0400

    change

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -12,7 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-          
+
+
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
